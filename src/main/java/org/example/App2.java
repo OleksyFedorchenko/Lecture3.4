ddba0cbee1a1a756c2d10501b5927e4f31fd8ed0
OleksyFedorchenko <otihiy@gmail.com>
2022-11-20 11:25:09 +0200

    adding Map<String,Double> as result

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -10,7 +10,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -19,6 +21,7 @@ public class App2 {
         ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
         List<Violation> violations = new ArrayList<>();
         List<Violation> temp;
+        Map<String,Double> result=new HashMap<>();
         List<String> jsonFileNames = getFileNames();
 
         //Читаємо усі файли і десеріалізуємо їх в коллекцію об'єктів
@@ -30,7 +33,27 @@ public class App2 {
                 violations.addAll(temp);
             }
         }
+
         System.out.println(violations.size());
+
+        //Робимо мапу з типами без дублікатів
+        for(Violation v:violations){
+            result.put(v.getType(),0.0);
+        }
+        System.out.println(result);
+
+        //Підраховуємо суму для кожного типу порушень,
+        // як результат отримуємо мапу з результатами завдання (Тип порушення: Сума штрафу).
+        for(String s:result.keySet()){
+            double sum=0.0;
+            for(Violation v:violations){
+                if(s.equals(v.type)){
+                    sum+=v.fineAmount;
+                }
+                result.put(s,sum);
+            }
+        }
+        System.out.println(result);
     }
 
     //Шукаємо всі json файли в поточному каталозі
