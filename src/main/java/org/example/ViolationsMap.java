84d755081860ffeb3b47a753bd6558d06b26161b
OleksyFedorchenko <otihiy@gmail.com>
2022-11-22 19:40:10 +0200

    refactoring code

--- src/main/java/org/example/ViolationsMap.java
+++ src/main/java/org/example/ViolationsMap.java
@@ -3,16 +3,16 @@ package org.example;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.*;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Getter
 @Setter
-@XmlRootElement(name = "violates")
+@XmlRootElement(name = "violations")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ViolationsMap {
+    @XmlElementWrapper(name="violation")
+    @XmlElement(name="element")
     private Map<String, Double> violate = new LinkedHashMap<>();
 }
